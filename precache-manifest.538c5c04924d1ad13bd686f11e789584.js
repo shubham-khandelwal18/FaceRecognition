d998b31256417e2657397c0b2dadcbcb49a2a36c
Shubham Khandelwal <shubhamsharma18082001@gmail.com>
2020-07-12 15:06:26 +0530

    Updates

---
+++ precache-manifest.538c5c04924d1ad13bd686f11e789584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25a146a67571b021cdcc78d70a90ff7",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "ec372d0d2eb2e634dd25",
+    "url": "/FaceRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "23a0dfb1e77346dc0dc6",
+    "url": "/FaceRecognition/static/css/main.8d7fc238.chunk.css"
+  },
+  {
+    "revision": "ec372d0d2eb2e634dd25",
+    "url": "/FaceRecognition/static/js/2.6893cd90.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceRecognition/static/js/2.6893cd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a0dfb1e77346dc0dc6",
+    "url": "/FaceRecognition/static/js/main.03412f2e.chunk.js"
+  },
+  {
+    "revision": "e25582aaa4b13542de36",
+    "url": "/FaceRecognition/static/js/runtime-main.cf3dbe61.js"
+  }
+]);
